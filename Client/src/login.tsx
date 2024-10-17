b5346f71764c9417af36a3b585958b2bcfeba849
DhanushPrakash J <dhanushprakash194@gmail.com>
2024-10-17 20:30:33 +0530

    Commit

--- Client/src/login.tsx
+++ Client/src/login.tsx
@@ -10,7 +10,7 @@ function Register() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const response = await fetch('https://fn-axiom-server.vercel.app/api/login', {
+    const response = await fetch('http://localhost:5000/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
