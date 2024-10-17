acc27ff3cb510afb804d8be31c74512c695d6d4c
DhanushPrakash J <dhanushprakash194@gmail.com>
2024-10-17 19:17:42 +0530

    Commit

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App.tsx'
-import Login from './login.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
+import Login from './login.tsx';
 import Register from './register.tsx';
-import './index.css'
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </StrictMode>
-)
+);
